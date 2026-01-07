708bd5ee021b852a166d9ae6a87f6240bcdb024d
kaviperarasu47 <kaviperarasukg@gmail.com>
2026-01-07 16:25:35 +0530

    Update problem7.py

--- problem 7/problem7.py
+++ problem 7/problem7.py
@@ -1,11 +1,13 @@
-n=input("Enter the item:")
-m=int(input("Enter the price of item:"))
-p=int(input("Enter the quantity:"))
-print("ITEM PURCHASED:",n)
-print("PRICE OF THE ITEM:",m)
-print("QUANTITY",p)
+no=int(input("Enter the number of items:"))
+for i in range(no):
+ n=input("Enter the item:")
+ m=int(input("Enter the price of item:"))
+ p=int(input("Enter the quantity:"))
+ print("ITEM PURCHASED:",n)
+ print("PRICE OF THE ITEM:",m)
+ print("QUANTITY",p)
 t=p*m
 print("TOTAL AMOUNT",t)
 d=10
 f=t-d
-print("FINAL AMOUNT",f)
+ print("FINAL AMOUNT",f)

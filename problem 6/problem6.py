41d358e476321898f0e47157cfdc4135fa63bb5f
kaviperarasu47 <kaviperarasukg@gmail.com>
2026-01-07 16:37:34 +0530

    Update problem6.py

--- problem 6/problem6.py
+++ problem 6/problem6.py
@@ -1,3 +1,21 @@
-n=input("Enter the password:")
-Isupper(n)
-
+p=input("Enter the password:")
+point=1
+special=[!,@,#,$,%,&]
+l=len(p)
+if (l>=8):
+    point+=1
+if (isupper(p)):
+    point+=1
+if(islower(p)):
+    point+=1
+if(isnumerical(p)):
+    point+=1
+for i in special:
+if (p=i):
+ point+=1
+if point<=2:
+print("Password is weak")
+if point<=4:
+print("Password is medium")
+if points=5:
+print("password is strong")
